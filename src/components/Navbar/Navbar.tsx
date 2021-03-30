401884ac52f5bf6371d27f852257e29b5e239c0f
makcsholonik <makcsholonik@gmail.com>
2021-03-30 21:44:23 +0300

    49 Users, UsersContainer (edited)

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,6 +12,9 @@ export const Navbar = () => {
             <div className={s.item}>
                 <NavLink to={"/dialogs"} activeClassName={s.active} >Messages</NavLink>
             </div>
+           <div className={s.item}>
+              <NavLink to={"/users"} activeClassName={s.active} >Users</NavLink>
+           </div>
             <div className={s.item}>
                 <NavLink to={"/new"} activeClassName={s.active} >News</NavLink>
             </div>
--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,32 +7,33 @@ export const Users = ( props : UsersPropsType ) => {
 
 	// если пользователей нет, только тогда их отрисовываем, чтобы не было зацикленности
 	if (props.userPage.users.length === 0) {
-	props.setUsers ( [
-		{
-			id : v1 (),
-			photoUrl : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXHvfFwNodq7RdvqxFOdob4l_mI4elb10pUQ&usqp=CAU',
-			followed : true,
-			fullName : 'Maxim',
-			status : 'active',
-			location : { id : v1 (), city : 'Kobrin', country : 'Belarus' }
-		},
-		{
-			id : v1 (),
-			photoUrl : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXHvfFwNodq7RdvqxFOdob4l_mI4elb10pUQ&usqp=CAU',
-			followed : true,
-			fullName : 'Ilya',
-			status : 'active',
-			location : { id : v1 (), city : 'Vienna', country : 'Austria' }
-		},
-		{
-			id : v1 (),
-			photoUrl : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXHvfFwNodq7RdvqxFOdob4l_mI4elb10pUQ&usqp=CAU',
-			followed : false,
-			fullName : 'Fedor',
-			status : 'inactive',
-			location : { id : v1 (), city : 'Berlin', country : 'Germany' }
-		}]
-	)}
+		props.setUsers ( [
+			{
+				id : v1 (),
+				photoUrl :  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXHvfFwNodq7RdvqxFOdob4l_mI4elb10pUQ&usqp=CAU',
+				followed : true,
+				fullName : 'Maxim',
+				status : 'active',
+				location : { id : v1 (), city : 'Kobrin', country : 'Belarus' }
+			},
+			{
+				id : v1 (),
+				photoUrl : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXHvfFwNodq7RdvqxFOdob4l_mI4elb10pUQ&usqp=CAU',
+				followed : true,
+				fullName : 'Ilya',
+				status : 'active',
+				location : { id : v1 (), city : 'Vienna', country : 'Austria' }
+			},
+			{
+				id : v1 (),
+				photoUrl : 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXHvfFwNodq7RdvqxFOdob4l_mI4elb10pUQ&usqp=CAU',
+				followed : false,
+				fullName : 'Fedor',
+				status : 'inactive',
+				location : { id : v1 (), city : 'Berlin', country : 'Germany' }
+			}]
+		)
+	}
 
 	return (
 		<div>
@@ -40,14 +41,14 @@ export const Users = ( props : UsersPropsType ) => {
 				props.userPage.users.map ( u => <div key={ u.id }>
 					<span>
 						<div>
-							<img src={ u.photoUrl } alt="freeman" className={ cl.userPhoto }/>
+							<img src={ u.photoUrl } alt='' className={ cl.userPhoto }/>
 						</div>
 						<div>
 							{ u.followed
 								? <button onClick={ () => {props.unFollow ( u.id )} }>Unfollow</button>
-								: <button onClick={ () => {props.follow ( u.id )} }>follow</button> }
+								: <button onClick={ () => {props.follow ( u.id )} }>Follow</button> }
 						</div>
-						</span>
+					</span>
 					<span>
 						<span>
 							<div>{ u.fullName }</div>
@@ -57,7 +58,7 @@ export const Users = ( props : UsersPropsType ) => {
 							<div>{ u.location.country }</div>
 							<div>{ u.location.city }</div>
 						</span>
-						</span>
+					</span>
 				</div> )
 			}
 		</div>
