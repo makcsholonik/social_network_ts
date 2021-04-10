5cd282bd84bd39f5919166a35e802d3b4e598aee
makcsholonik <makcsholonik@gmail.com>
2021-04-10 16:24:30 +0300

    55 pagination

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -3,23 +3,38 @@ import { Users } from "./Users";
 import { connect } from "react-redux";
 import { AppStateType } from "../../redux/redux-store";
 import { Dispatch } from "redux";
-import { followAC, InitialStateUserType, setUsersAC, unFollowAC, UserType } from "../../redux/usersReducer";
+import {
+	followAC,
+	InitialStateUserType,
+	setCurrentPageAC, setTotalUserCountsAC,
+	setUsersAC,
+	unFollowAC,
+	UserType
+} from "../../redux/usersReducer";
 
 type MapStatePropsType = {
 	userPage : InitialStateUserType
+	pageSize : number
+	totalUsersCount : number
+	currentPage : number
 }
 
 type MapDispatchPropsType = {
 	follow : ( userId : string ) => void
 	unFollow : ( userId : string ) => void
 	setUsers : ( users : Array<UserType> ) => void
+	setCurrentPage: (pageNumber: number ) => void
+	setTotalUserCounts : ( totalCount: number ) => void
 }
 
 export type UsersPropsType = MapStatePropsType & MapDispatchPropsType
 
 const mapStateToProps = ( state : AppStateType ) : MapStatePropsType => {
 	return {
-		userPage : state.usersPage
+		userPage : state.usersPage,
+		pageSize : state.usersPage.pageSize,
+		totalUsersCount : state.usersPage.totalUsersCount,
+		currentPage : state.usersPage.currentPage
 	}
 }
 const mapDispatchToProps = ( dispatch : Dispatch ) : MapDispatchPropsType => {
@@ -32,6 +47,12 @@ const mapDispatchToProps = ( dispatch : Dispatch ) : MapDispatchPropsType => {
 		},
 		setUsers : ( users : Array<UserType> ) => {
 			dispatch ( setUsersAC ( users ) )
+		},
+		setCurrentPage : ( pageNumber: number ) => {
+			dispatch ( setCurrentPageAC ( pageNumber ) )
+		},
+		setTotalUserCounts : ( totalCount: number ) => {
+			dispatch ( setTotalUserCountsAC ( totalCount ) )
 		}
 	}
 }
--- src/redux/usersReducer.ts
+++ src/redux/usersReducer.ts
@@ -14,7 +14,10 @@ export type UserType = {
 }
 
 const initialState = {
-	users : [] as Array<any>
+	users : [] as Array<any>,
+	pageSize : 10, // кол-во пользователей на 1 стр.
+	totalUsersCount : 0, // всего пользователей
+	currentPage : 1 // текущая страница пришедшая с сервера
 }
 
 export type InitialStateUserType = typeof initialState;
@@ -23,6 +26,8 @@ export type UsersReducerActionType =
 	ReturnType<typeof followAC>
 	| ReturnType<typeof unFollowAC>
 	| ReturnType<typeof setUsersAC>
+	| ReturnType<typeof setCurrentPageAC>
+	| ReturnType<typeof setTotalUserCountsAC>
 
 export const usersReducer = ( state : InitialStateUserType = initialState, action : UsersReducerActionType ) : InitialStateUserType => {
 	switch (action.type) {
@@ -45,7 +50,11 @@ export const usersReducer = ( state : InitialStateUserType = initialState, actio
 				} )
 			}
 		case SET_USERS:
-			return { ...state, users : [...state.users, ...action.users] }
+			return { ...state, users : action.users }
+		case SET_CURRENT_PAGE:
+			return { ...state, currentPage : action.currentPage }
+		case SET_TOTAL_USER_COUNT:
+			return {...state, totalUsersCount : action.totalCount}
 		default:
 			return state
 		// делаем копию старых users которые были в массиве и добавляем к ним users которые пришли из action
@@ -55,7 +64,11 @@ export const usersReducer = ( state : InitialStateUserType = initialState, actio
 export const FOLLOW = 'FOLLOW';
 export const UNFOLLOW = 'UNFOLLOW';
 export const SET_USERS = 'SET-USERS';
+export const SET_CURRENT_PAGE = 'SET-CURRENT-PAGE';
+export const SET_TOTAL_USER_COUNT = 'SET-TOTAL-USER-COUNT';
 
 export const followAC = ( userId : string ) => ({ type : FOLLOW, userId } as const);
 export const unFollowAC = ( userId : string ) => ({ type : UNFOLLOW, userId } as const);
 export const setUsersAC = ( users : Array<UserType> ) => ({ type : SET_USERS, users } as const);
+export const setCurrentPageAC = ( currentPage : number ) => ({ type : SET_CURRENT_PAGE, currentPage } as const);
+export const setTotalUserCountsAC = ( totalCount : number ) => ({ type : SET_TOTAL_USER_COUNT, totalCount } as const);
