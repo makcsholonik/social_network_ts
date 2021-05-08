da72a469b430505039e8b66bbadeba9fdefcc14e
makcsholonik <makcsholonik@gmail.com>
2021-05-09 00:22:37 +0300

    76 redux-form (ts)

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,14 +3,16 @@ import s from "./Dialogs.module.css";
 import { DialogMessage } from "./DialogMessage/DialogMessage";
 import { DialogItemUser } from "./DialogItem/DialogItem";
 import { DialogsPropsType } from "./DialogsContainer";
-import { Field, reduxForm } from "redux-form";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
+
+
 export const Dialogs = ( props : DialogsPropsType ) => {
 
 	let dialogsElements = props.dialogs.map ( d => <DialogItemUser key={ d.id } id={ d.id } name={ d.name }/> );
 	let messagesElements = props.messages.map ( m => <DialogMessage key={ m.id } message={ m.message }/> );
 
-	let addNewMessage = ( values : any ) => {
-		props.onSendMessageClick (values.newMessageBody)
+	let addNewMessage = ( formData : FormDataType ) => {
+		props.onSendMessageClick (formData.newMessageBody)
 	}
 
 	// * Если мы не залогинены нас вернёт на страницу login
@@ -29,7 +31,11 @@ export const Dialogs = ( props : DialogsPropsType ) => {
 	)
 }
 
-export const AddMessageForm = ( props : any ) => {
+type FormDataType = {
+	newMessageBody: string
+}
+
+export const AddMessageForm = ( props : InjectedFormProps<FormDataType> ) => {
 	return (
 		<form onSubmit={ props.handleSubmit }>
 			<Field component={ "textarea" } placeholder={ "Enter your message" } name={ "newMessageBody" }/>
@@ -37,4 +43,4 @@ export const AddMessageForm = ( props : any ) => {
 		</form>
 	)
 }
-const AddMessageFormRedux = reduxForm<any> ( { form : "addMessageForm" } ) ( AddMessageForm )
+const AddMessageFormRedux = reduxForm<FormDataType> ( { form : "addMessageForm" } ) ( AddMessageForm )
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import { MyPostPropsType } from './MyPostsContainer';
 import { Post } from "./Post/Post";
-import { Field, reduxForm } from "redux-form";
+import { Field, InjectedFormProps, reduxForm } from "redux-form";
 
 export const MyPosts = ( props : MyPostPropsType ) => {
 
 	let postsElements = props.posts.map ( p => <Post message={ p.message } likeCounts={ p.likeCounts }/> )
 
-	let addNewPost = (values: any) => {
-		props.addPost(values.newPostBody)
+	let addNewPost = (formData: FormDataType) => {
+		props.addPost(formData.newPostBody)
 	}
 
 	return (
@@ -21,7 +21,11 @@ export const MyPosts = ( props : MyPostPropsType ) => {
 	);
 }
 
-export const AddPostForm = ( props : any ) => {
+type FormDataType = {
+	newPostBody: string
+}
+
+export const AddPostForm = ( props : InjectedFormProps<FormDataType> ) => {
 	return (
 		<form onSubmit={ props.handleSubmit }>
 			<Field component={ "textarea" } placeholder={ "Enter your post" } name={ "newPostBody" }/>
@@ -30,4 +34,4 @@ export const AddPostForm = ( props : any ) => {
 	)
 }
 
-const AddPostFormRedux = reduxForm<any> ({form: "addPostForm"})(AddPostForm)
+const AddPostFormRedux = reduxForm<FormDataType> ({form: "addPostForm"})(AddPostForm)
