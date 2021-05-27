11030612456df6c5d78fa11d0cb877c852354ea2
makcsholonik <makcsholonik@gmail.com>
2021-05-27 22:06:55 +0300

    83 reselect

--- src/redux/usersSelectors.ts
+++ src/redux/usersSelectors.ts
@@ -1,7 +1,7 @@
 import { AppStateType } from "./redux-store";
 import { createSelector } from "reselect";
 
-export const getUsersSelector = ( state : AppStateType ) => {
+const getUsersSelector = ( state : AppStateType ) => {
 	return state.usersPage.users
 }
 export const getUsers = createSelector ( getUsersSelector, ( users ) => {
